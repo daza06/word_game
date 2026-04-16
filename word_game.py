c96355df99b42cc96389c5ce4ed366359a0b4e36
Daniel Zavala <link123852@gmail.com>
2026-04-16 01:57:25 -0600

    setup repository and create program layout

---
+++ word_game.py
@@ -0,0 +1,42 @@
+import csv
+import random
+
+
+def select_word():
+    """
+    read from a csv and pick a random word from a list of five letter words
+    parameters: none
+    return: word
+
+    """
+    pass
+
+
+def user_guess():
+    """
+    ask user for a guess, word must be five letters and must be letters and not numbers
+    parameters: none
+    return: guess
+
+    """
+    pass
+
+
+def check_guess():
+    """
+    compares the secret word to the user guess and displays whether there is
+    a letter in the correct position, wrong position or not in the secret word
+    parameters: secret_word, guess
+    returns: check_letter
+
+    """
+    pass
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
